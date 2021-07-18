7b7b74831ef5df25f6d53a82382c6228762892d7
Hui Ling <huilingw312@gmail.com>
2021-07-18 12:20:35 +0100

    There is a bug

--- miller_test.py
+++ miller_test.py
@@ -143,6 +143,24 @@ if __name__ == "__main__":
 
     # print("{}".format(base.get_base()))
 
-    for i in range(2, 30):
-        print("i = {} is prime = {}".format(i, is_prime_miller_test(i)))
+    oeis_primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,
+        61,67,71,73,79,83,89,97,101,103,107,109,113,127,
+        131,137,139,149,151,157,163,167,173,179,181,191,
+        193,197,199,211,223,227,229,233,239,241,251,257,
+        263,269,271]
+
+    my_primes = []
+
+    for i in range(2, 300):
+        if is_prime_miller_test(i):
+            my_primes.append(i)
+            print("i = {} is prime ".format(i, ))
         # print("")
+
+    for prime in my_primes:
+        if prime not in oeis_primes:
+            print("{} identified as prime but isn't".format(prime))
+
+    for prime in oeis_primes:
+        if prime not in my_primes:
+            print("{} is prime but not identified".format(prime))
