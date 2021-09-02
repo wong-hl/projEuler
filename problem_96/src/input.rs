43dcd4927393f42e27c7654488c2d9fedfd3b4e2
Hui Ling <huilingw312@gmail.com>
2021-09-02 22:11:29 +0100

    Fixed assertion

--- problem_96/src/input.rs
+++ problem_96/src/input.rs
@@ -1,6 +1,8 @@
-use std::{fs::File, io::prelude::*, path::Path};
+use std::{collections::HashMap, fs::File, io::prelude::*, path::Path};
+use ndarray::Array2;
 
-fn read_from_file(file_name: String) -> String {
+
+fn read_from_file(file_name: String) -> Result<String, String> {
     // https://doc.rust-lang.org/stable/rust-by-example/std_misc/file/open.html
 
     let input_path: &Path = Path::new(&file_name);
@@ -16,12 +18,22 @@ fn read_from_file(file_name: String) -> String {
     let mut file_contents = String::new();
     match file.read_to_string(&mut file_contents) {
         Err(why) => panic!("couldn't read {}: {}", file_display, why),
-        Ok(_) => file_contents
+        Ok(_) => Ok(file_contents),
     }
 
 }
 
+fn process_input(file_name: String, num_puzzles: usize) -> Result<HashMap<u8, Array2<u8>>, String> {
+    let file_contents = read_from_file(file_name);
+
+    let sorted_input = HashMap::with_capacity(num_puzzles);
+
+    Ok(sorted_input)
+}
+
 #[test]
 fn path_to_file_works() {
-    assert_eq!(false, read_from_file(String::from("p096_sudoku.txt")).is_empty());
+    if let Ok(output) = read_from_file(String::from("p096_sudoku.txt")) {
+        assert!(!output.is_empty());
+    }
 }
