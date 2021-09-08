c83e9b00686fbb3d18875f0570b6af71ca0cc3dd
Hui Ling <huilingw312@gmail.com>
2021-09-08 16:56:50 +0100

    Added reference for method on preemptive sets

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -6,6 +6,9 @@
 
             As it was meant as a prototype, best practices have not been adhered to
             and object oriented programming wasn't used. 
+
+    Reference for pre-emptive sets:
+        http://www.ams.org/notices/200904/rtx090400460p.pdf
 '''
 
 
