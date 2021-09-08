6be51f6faa0153cbf1424b600659b2a71231ed20
Hui Ling <huilingw312@gmail.com>
2021-09-08 16:53:24 +0100

    Added some comments

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -11,6 +11,7 @@ def find_trivial_solutions(solutions, which_subset):
     num_solns = 0
 
     for index in range(9):
+        # Get relevant section of data
         if which_subset == "square":
             row_index = (index // 3) * 3
             col_index = (index % 3) * 3
@@ -25,19 +26,28 @@ def find_trivial_solutions(solutions, which_subset):
 
         for index, _ in enumerate(subset):
 
+            # A candidate solution (contains several values)
             candidate = subset[index]
 
+            # len() == 1 implies solution fixed
             if len(candidate) == 1:
                 continue
 
+            # Remove candidate from subset of information
             this_subset = np.delete(subset, index)
 
+            # Find intersection between candidate and all other values in subset
             vals_in_other_sets = [x & candidate for x in this_subset]
+
+            # If list is not empty 
             if vals_in_other_sets:
+                # Find set containing all values of candidate in subset of info 
                 in_other_sets = set.union(*vals_in_other_sets)
+                # Find value that is not in rest of subset
                 unique = candidate - set(in_other_sets)
 
                 if unique:
+                    # If present, it is a trivial solution 
                     subset[index] = unique
                     num_solns += 1
 
@@ -54,18 +64,46 @@ def find_trivial_solutions(solutions, which_subset):
 
     return solutions, num_solns
 
+# def find_trivial_solutions(solutions):
+
+#     num_solns = 0
+
+#     for row_index in range(9):
+#         for col_index in range(9):
+#             # A candidate solution (contains several values)
+#             candidate = solutions[row_index, col_index]
+
+#             # len() == 1 implies solution fixed
+#             if len(candidate) == 1:
+#                 continue
+
+#             set_of_related_vals = get_associated_vals(solutions, row_index, col_index, only_fixed_soln=False)
+
+#             difference = candidate - set_of_related_vals 
+
+#             if len(difference) == 1:
+#                 solutions[row_index, col_index] = difference
+#                 num_solns += 1
+
+#     solutions = update_candidate_solutions(solutions)
+
+#     return solutions, num_solns
+            
+
 
 def update_candidate_solutions(solutions):
+    '''
+        Removes any solution values from a given cell
+    '''
     for row_index in range(9):
         for col_index in range(9):
-
             cell_val = solutions[row_index, col_index]
-
             if len(cell_val) != 1:
-
+                # Get set of values related to this cell
                 fixed_solutions = get_associated_vals(solutions, row_index, col_index)
-
+                # Deteremine if there is an intersection between current cell and solved solutions
                 if cell_val & fixed_solutions:
+                    # Remove any intersection as those values should noe be in the cell
                     difference = cell_val - fixed_solutions
                     if difference:
                         solutions[row_index, col_index] = difference
@@ -76,6 +114,9 @@ def update_candidate_solutions(solutions):
 
 
 def update_subset_preemptive(mask, candidate_set_subset, preemptive_set):
+    '''
+        Removes values in pre emptive set from terms not in said set
+    '''
     return np.asarray(
         [
             val - (val & preemptive_set) if len(val) != 1 and not in_set else val
@@ -97,8 +138,12 @@ def solve_preemptive_sets(solutions):
             if cell_len == 1:
                 continue
 
+            # Finds how many sets in that row are a subset of this cell
             row_mask = solutions[row_index, :] <= cell_val
 
+            # Uses definition of preemptive set
+            # If the number of terms in the set is the same as the number of cells
+            # that are subsets of current cell => pre emptive set
             if row_mask.sum() == cell_len:
                 solutions[row_index, :] = update_subset_preemptive(
                     row_mask, solutions[row_index, :], cell_val
@@ -106,8 +151,8 @@ def solve_preemptive_sets(solutions):
                 solutions = update_candidate_solutions(solutions)
                 num_sets_found += 1
 
+            # Repeat for column
             col_mask = solutions[:, column_index] <= cell_val
-
             if col_mask.sum() == cell_len:
                 solutions[:, column_index] = update_subset_preemptive(
                     col_mask, solutions[:, column_index], cell_val
@@ -115,10 +160,10 @@ def solve_preemptive_sets(solutions):
                 solutions = update_candidate_solutions(solutions)
                 num_sets_found += 1
 
+            # Repeat for sector
             sector_mask = (
                 solutions[row_sec : row_sec + 3, col_sec : col_sec + 3] <= cell_val
             )
-
             if sector_mask.sum() == cell_len:
                 sector = solutions[
                     row_sec : row_sec + 3, col_sec : col_sec + 3
@@ -155,6 +200,10 @@ def is_sufficiently_solved(solutions):
 
 
 def find_candidate_solutions(puzzle, master_set):
+    '''
+        From the initial soduku puzzle provided, find all candidate solutions
+        for every cell in the grid
+    '''
     candidate_solutions = np.empty((9, 9), dtype=set)
 
     for row_index in range(9):
@@ -169,6 +218,8 @@ def find_candidate_solutions(puzzle, master_set):
             else:
                 col_sec = (column_index // 3) * 3
                 row_sec = (row_index // 3) * 3
+                # Uses the condition that the value present in this cell, cannot be
+                # present in all other related cells
                 candidate_solutions[row_index, column_index] = master_set - (
                     row_set
                     | set(puzzle[:, column_index])
@@ -227,24 +278,40 @@ def check_solution(solution):
     return True
 
 
-def get_associated_vals(solution, row_index, column_index):
+def get_associated_vals(solution, row_index, column_index, only_fixed_soln = True):
     row_sec = (row_index // 3) * 3
     col_sec = (column_index // 3) * 3
 
-    row_vals = [
-        z for z in np.delete(solution[row_index, :], column_index) if len(z) == 1
-    ]
-    col_vals = [
-        z for z in np.delete(solution[:, column_index], row_index) if len(z) == 1
-    ]
-    sector_vals = [
-        z
-        for z in np.delete(
-            solution[row_sec : row_sec + 3, col_sec : col_sec + 3].flatten(),
-            (row_index - row_sec) * 3 + (column_index - col_sec),
-        )
-        if len(z) == 1
-    ]
+    if only_fixed_soln:
+        row_vals = [
+            z for z in np.delete(solution[row_index, :], column_index) if len(z) == 1
+        ]
+        col_vals = [
+            z for z in np.delete(solution[:, column_index], row_index) if len(z) == 1
+        ]
+        sector_vals = [
+            z
+            for z in np.delete(
+                solution[row_sec : row_sec + 3, col_sec : col_sec + 3].flatten(),
+                (row_index - row_sec) * 3 + (column_index - col_sec),
+            )
+            if len(z) == 1
+        ]
+    else:
+        row_vals = [
+            z for z in np.delete(solution[row_index, :], column_index) 
+        ]
+        col_vals = [
+            z for z in np.delete(solution[:, column_index], row_index) 
+        ]
+        sector_vals = [
+            z
+            for z in np.delete(
+                solution[row_sec : row_sec + 3, col_sec : col_sec + 3].flatten(),
+                (row_index - row_sec) * 3 + (column_index - col_sec),
+            )
+        ]
+
 
     if row_vals:
         row = set.union(*row_vals)
@@ -266,6 +333,7 @@ def get_associated_vals(solution, row_index, column_index):
 
 def puzzle_three_digit_num(solution_vals):
     multiplier = [100, 10, 1]
+    # pop used to extract value from set
     return sum([val.pop() * mult for val, mult in zip(solution_vals, multiplier)])
 
 
@@ -291,7 +359,7 @@ def extract_puzzles_from_file(file_path):
             row_counter += 1
 
 
-def solve_for_trivial_solutions(current_solutions, is_solved):
+def solve_for_trivial_solutions(current_solutions, is_solved, subset_names):
     total_no_soln = 0
     total_iters = 0
 
@@ -300,6 +368,9 @@ def solve_for_trivial_solutions(current_solutions, is_solved):
         current_solutions, num_sol_found = find_trivial_solutions(
             current_solutions, subset_names[counter]
         )
+        # current_solutions, num_sol_found = find_trivial_solutions(
+        #     current_solutions
+        # )
         is_solved = is_completely_solved(current_solutions)
 
         if num_sol_found == 0:
@@ -342,12 +413,17 @@ def propagate_guess(previous_node, depth_of_guess):
                 found_valid = True
                 break
 
+        # If all children nodes are not valid
         if not found_valid:
+            # Set that this node is not valid
             previous_node.is_valid = False
+            # "Remove" this solution
             previous_node.soln = None
+            # Go up once in the ancestory
             previous_node = previous_node.parent
             depth_of_guess -= 1
 
+            # If it is the root node, there is no parent
             if previous_node is None:
                 break
 
@@ -373,6 +449,7 @@ def increment_guessing_index(row_index, col_index):
 def is_guess_valid(current_solution, row_index, column_index, guess_val):
     fixed_vals = get_associated_vals(current_solution, row_index, column_index)
 
+    # If guess value is present in solved values => guess is not valid
     if fixed_vals & {guess_val}:
         return False
     else:
@@ -381,12 +458,14 @@ def is_guess_valid(current_solution, row_index, column_index, guess_val):
 
 def solve_using_guessing(current_solutions, is_solved):
     guess_depth = 0
+    # Initialise previous node - root of guessing tree
     prev_node = AnyNode(guess_val=None, soln=current_solutions)
     # root = prev_node
 
     row_index = 0
     column_index = 0
 
+    # Use while loop instead of for as iteration is not linear
     while row_index < 9 and column_index < 9 and not is_solved:
         cell_val = current_solutions[row_index, column_index]
         cell_len = len(cell_val)
@@ -395,14 +474,15 @@ def solve_using_guessing(current_solutions, is_solved):
             row_index, column_index = increment_guessing_index(row_index, column_index)
             continue
 
+        # Convert to list to iterate over values
         cell_val = list(cell_val)
 
-        curr_solution = deepcopy(current_solutions)
-
         for val in cell_val:
-            guess = deepcopy(curr_solution)
+            # Deep copy to protest original solution
+            guess = deepcopy(current_solutions)
 
             if not is_guess_valid(guess, row_index, column_index, val):
+                # If guess is not valid, update tree and go to next iteration
                 AnyNode(
                     parent=prev_node,
                     guess_val=val,
@@ -413,9 +493,12 @@ def solve_using_guessing(current_solutions, is_solved):
 
             guess[row_index, column_index] = {val}
 
+            # Using guess, attempt to update solution
             try:
                 temp = update_candidate_solutions(guess)
             except Exception:
+                # Exception raised => invalid solution exists
+                # Update tree and go to next iteration
                 AnyNode(
                     parent=prev_node,
                     guess_val=val,
@@ -424,7 +507,7 @@ def solve_using_guessing(current_solutions, is_solved):
                 )
                 continue
             else:
-                current_solutions = temp
+                # A valid solution is found
                 AnyNode(
                     parent=prev_node,
                     guess_val=val,
@@ -433,13 +516,16 @@ def solve_using_guessing(current_solutions, is_solved):
                     soln=temp,
                 )
 
+                # Check if this results in a solved puzzle
                 is_solved = is_completely_solved(temp)
 
                 if is_solved:
+                    # Initiate exiting of function
                     current_solutions = temp
                     found_valid = True
                     break
 
+        # Propagate guess if not solved 
         (
             current_solutions,
             row_index,
@@ -448,8 +534,6 @@ def solve_using_guessing(current_solutions, is_solved):
             guess_depth,
             prev_node,
         ) = propagate_guess(prev_node, guess_depth)
-
-        is_solved = is_completely_solved(current_solutions)
 
         if not found_valid:
             # print(RenderTree(root))
@@ -477,13 +561,8 @@ if __name__ == "__main__":
 
         candidate_solutions = find_candidate_solutions(puzzle, master_set)
 
-        total_no_soln = 0
-        total_iters = 0
-
-        solved = is_completely_solved(candidate_solutions)
-
         candidate_solutions, solved = solve_for_trivial_solutions(
-            candidate_solutions, solved
+            candidate_solutions, False, subset_names
         )
 
         if not solved:
@@ -499,7 +578,6 @@ if __name__ == "__main__":
         if solved:
             check_solution(candidate_solutions)
             puzzle_sum = puzzle_three_digit_num(candidate_solutions[0, 0:3])
-            # print(puzzle_sum)
             total_sum += puzzle_sum
 
     print(f"Total sum: {total_sum}")
