a64c1469c73437aa88e722a771df926af0b02372
Hui Ling <huilingw312@gmail.com>
2021-09-09 15:24:12 +0100

    Testing of more functions

--- problem_96/src/main.rs
+++ problem_96/src/main.rs
@@ -1,8 +1,20 @@
-mod input;
+
+pub mod input;
+mod sudoku;
 
 fn main() {
     let sudoku_problems =
         input::process_input(String::from("p096_sudoku.txt"), 50, String::from("Grid")).unwrap();
 
-    println!("{:?}", sudoku_problems);
+    println!("{:?}", sudoku_problems.get(&1));
+
+    if let Some(first_puzzle) =  sudoku_problems.get(&1) {
+        let row_val = sudoku::get_row_values(0, first_puzzle);
+        let col_val = sudoku::get_column_values(0, first_puzzle);
+        let sec_val = sudoku::get_sector_values(0, first_puzzle);
+        println!("{:?}, {:?} {:?}", row_val, col_val, sec_val);
+
+        let test = sudoku::process_sudoku_vector(first_puzzle);
+        println!("{:?}", test);
+    } 
 }
