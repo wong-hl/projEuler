cf3aaec553a4563ddc78d961aa21e4cd17baacf7
Hui Ling <huilingw312@gmail.com>
2021-09-16 14:03:22 +0100

    Brought in more functions

--- problem_87/src/main.rs
+++ problem_87/src/main.rs
@@ -1,8 +1,12 @@
 use crate::primes::prime_numbers_upto;
 
 pub mod primes;
+pub mod solver;
 
 fn main() {
-    let prime_numbers = prime_numbers_upto(100);
+    let upper_limit: u32 = 50;
+    let prime_limits = solver::PrimeBaseLimits::new(upper_limit);
+    println!("{:?}", prime_limits);
+    let prime_numbers = prime_numbers_upto(prime_limits.get_square() + 1);
     println!("{:?}", prime_numbers);
 }
