3f852e647688e191a581c4875a4f745e539ed169
Hui Ling <huilingw312@gmail.com>
2021-09-16 13:28:04 +0100

    Accept generic input

--- problem_87/src/primes.rs
+++ problem_87/src/primes.rs
@@ -1,26 +1,33 @@
-// use num::Integer;
+use num::ToPrimitive;
 
-fn seive(n: usize) -> Vec<bool> {
-    // Initialise aray of boolean values from 2 to n (shifted in implementation)
-    // i.e. index 0 = 2, index 1 = 3, ... etc
-    let mut out = vec![true; n - 2];
+fn seive<T>(n: T) -> Vec<bool>
+where
+    T: ToPrimitive,
+{
+    if let Some(max_val) = n.to_usize() {
+        // Initialise aray of boolean values from 2 to n (shifted in implementation)
+        // i.e. index 0 = 2, index 1 = 3, ... etc
+        let mut out = vec![true; max_val - 2];
 
-    // Max iteration number = sqrt(n)
-    let n_sqrt: usize = ((n as f64).sqrt() + 1.0) as usize;
+        // Max iteration number = sqrt(n)
+        let n_sqrt: usize = ((max_val as f64).sqrt() + 1.0) as usize;
 
-    // For i = 2, ..., sqrt(n)
-    (2..n_sqrt).for_each(|index| {
-        // if that index is true
-        if let Some(true) = out.get(index - 2) {
-            // Iterate over j = i^2, i^2 + i, ..., n
-            let max_k: usize = (n - index * index - 1) / index;
-            (0..=max_k).for_each(|k| out[index * index + k * index - 2] = false);
-        }
-    });
-    out
+        // For i = 2, ..., sqrt(n)
+        (2..n_sqrt).for_each(|index| {
+            // if that index is true
+            if let Some(true) = out.get(index - 2) {
+                // Iterate over j = i^2, i^2 + i, ..., n
+                let max_k: usize = (max_val - index * index - 1) / index;
+                (0..=max_k).for_each(|k| out[index * index + k * index - 2] = false);
+            }
+        });
+        out
+    } else {
+        panic!("Unable to convert to usize")
+    }
 }
 
-pub fn prime_numbers_upto(maximum_num: usize) -> Vec<usize> {
+pub fn prime_numbers_upto<T: ToPrimitive>(maximum_num: T) -> Vec<usize> {
     seive(maximum_num)
         .iter()
         .enumerate()
