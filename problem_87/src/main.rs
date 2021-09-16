f66ac12a1af642756d4e6cf723c7372121cae929
Hui Ling <huilingw312@gmail.com>
2021-09-16 14:09:32 +0100

    Solved!

--- problem_87/src/main.rs
+++ problem_87/src/main.rs
@@ -1,12 +1,15 @@
-use crate::primes::prime_numbers_upto;
+use crate::{primes::prime_numbers_upto, solver::prime_power_triples_solutions};
 
 pub mod primes;
 pub mod solver;
 
 fn main() {
-    let upper_limit: u32 = 50;
+    let upper_limit: u32 = 50000000;
     let prime_limits = solver::PrimeBaseLimits::new(upper_limit);
-    println!("{:?}", prime_limits);
+    // println!("{:?}", prime_limits);
     let prime_numbers = prime_numbers_upto(prime_limits.get_square() + 1);
-    println!("{:?}", prime_numbers);
+    // println!("{:?}", prime_numbers);
+    let soln = prime_power_triples_solutions(prime_limits, prime_numbers, upper_limit);
+    // println!("{:?}", soln);
+    println!("Num solutions: {}", soln.len());
 }
