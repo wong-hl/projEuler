77ae507c293c73635123c763ff1e879f9eb7021d
Hui Ling Wong <huiling.wong@thehutgroup.com>
2022-03-29 20:59:30 +0100

    Made print statements more verbose

--- problems-1-10/problem_1.rs
+++ problems-1-10/problem_1.rs
@@ -1,11 +1,11 @@
 fn main() {
     let s_3 = sum_of_multiples(3, 999);
-    println!("{}", s_3);
+    println!("Sum of multiples of 3: {}", s_3);
     let s_5 = sum_of_multiples(5, 999);
-    println!("{}", s_5);
+    println!("Sum of multiples of 5: {}", s_5);
     let s_15 = sum_of_multiples(15, 999);
-    println!("{}", s_15);
-    println!("{}", s_3 + s_5 - s_15);
+    println!("Sum of multiples of 15: {}", s_15);
+    println!("Sum of multiples of 3 or 15 below 1000: {}", s_3 + s_5 - s_15);
 }
 
 fn sum_of_multiples(multiple: u32, upper_bound: u32) -> u32 {
