e9bd9a002e4d62e2ee983518dc11da82a2bc7fc9
Hui Ling Wong <huiling.wong@thehutgroup.com>
2022-03-30 19:12:40 +0100

    Formatted file

--- problems-1-10/problem_1.rs
+++ problems-1-10/problem_1.rs
@@ -5,7 +5,10 @@ fn main() {
     println!("Sum of multiples of 5: {}", s_5);
     let s_15 = sum_of_multiples(15, 999);
     println!("Sum of multiples of 15: {}", s_15);
-    println!("Sum of multiples of 3 or 15 below 1000: {}", s_3 + s_5 - s_15);
+    println!(
+        "Sum of multiples of 3 or 15 below 1000: {}",
+        s_3 + s_5 - s_15
+    );
 }
 
 fn sum_of_multiples(multiple: u32, upper_bound: u32) -> u32 {
