c6b0fb7a23be2111d2e98d6bd8eee2f07b3d8c0c
Hui Ling Wong <huiling.wong@thehutgroup.com>
2022-03-31 14:48:52 +0100

    Added explicit types

--- problems-1-10/problems-1-10/src/bin/problem_3.rs
+++ problems-1-10/problems-1-10/src/bin/problem_3.rs
@@ -3,22 +3,22 @@ fn main() {
     println!("{:?}", &sieve);
 }
 
-fn sieve_of_eratosthenes(max_value: u32) -> Vec<bool> {
+fn sieve_of_eratosthenes(max_value: u64) -> Vec<bool> {
     let max_val: usize = max_value as usize;
     let mut is_prime: Vec<bool> = vec![true; max_val - 2];
     let sqrt_n: usize = (max_value as f64).sqrt().ceil() as usize;
 
-    (2..sqrt_n).for_each(|index| {
+    (2..sqrt_n).for_each(|index: usize| {
         if let Some(true) = is_prime.get(index - 2) {
-            (0..=((max_val - index * index - 1) / index))
-                .for_each(|k| is_prime[index * index + k * index - 2] = false);
+            let max_k: usize = (max_val - index * index - 1) / index;
+            (0..=max_k).for_each(|k: usize| is_prime[index * index + k * index - 2] = false);
         }
     });
 
     is_prime
 }
 
-fn primes_up_to(max_value: u32) -> Vec<usize> {
+fn primes_up_to(max_value: u64) -> Vec<usize> {
     sieve_of_eratosthenes(max_value)
         .iter()
         .enumerate()
